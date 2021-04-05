4a23838011a3919543f0bb5d80628f9a3a47ad60
yosoyjacko <jorge_a.correa@hotmail.com>
2021-04-05 08:56:21 -0300

    creando el list del from, en consola se ve

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import TodoForm from './TodoForm';
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return;
+        }
+
+
+        const newTodos = [todo, ...todos];
+
+        setTodos(newTodos);
+        console.log(...todos);
+    };
+    return (
+        <div>
+            <h1>What's the Plan for Today?/ Que Plan hay para Hoy</h1>
+            <TodoForm onSubmit={addTodo} />
+        </div>
+    )
+}
+
+export default TodoList;
